fbbe1cab8dc0c61a9636059cd38be4913fdf328a
Naaylla <lulita181910@hotmail.fr>
2024-07-09 05:00:08 +0100

    fixed email already used display message

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -4,11 +4,17 @@ const bcrypt = require("bcrypt");
 const User = require('../models/User');
 
 router.get("/", function (req, res) {
-    res.render("register.ejs");
+    res.render("register.ejs", { errorMessage: req.flash('errorMessage') });
 });
 
 router.post("/", async function (req, res) {
     try {
+        const existingUser = await User.findOne({ email: req.body.email });
+        if (existingUser) {
+            req.flash('errorMessage', 'Email is already used.');
+            return res.redirect("/register");
+        }
+
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
         const newUser = new User({
             name: req.body.name,
@@ -20,6 +26,7 @@ router.post("/", async function (req, res) {
         res.redirect("/");
     } catch (e) {
         console.error(e);
+        req.flash('errorMessage', 'An error occurred. Please try again.');
         res.redirect("/register");
     }
 });
